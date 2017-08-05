6849c2cc610dfd5e211d355cba8c76fecb16b722
Ivar Nilsen <ivar.nilsen@sparebank1.no>
2017-08-05 12:41:34 +0200

    SSL and compression

--- server/index.js
+++ server/index.js
@@ -4,17 +4,15 @@ const fallback = require('express-history-api-fallback');
 
 const app = express();
 
-/*
 if (process.env.NODE_ENV === 'production') {
     const enforce = require('express-sslify');
     const compression = require('compression');
     app.use(enforce.HTTPS({ trustProtoHeader: true }));
     app.use(compression());
 }
-*/
 
 const root = path.join(__dirname, '..', 'dist');
-console.log(root)
+
 app.use(express.static(root));
 app.use(fallback('index.html', { root }));
 
