f772c8e401eeff0865f29bd4b7fa4de404a5b9b2
Ivar Nilsen <ivar.nilsen@sparebank1.no>
2017-08-05 14:06:35 +0200

    Revert "See if sabayon still works"
    This reverts commit 80277b85628e767ba4ac5636b514a8ac9579afce.

--- server/index.js
+++ server/index.js
@@ -13,29 +13,6 @@ if (process.env.NODE_ENV === 'production') {
 
 const root = path.join(__dirname, '..', 'dist');
 
-app.get('/.well-known/acme-challenge/:acmeToken', function(req, res, next) {
-  var acmeToken = req.params.acmeToken;
-  var acmeKey;
-
-  if (process.env.ACME_KEY && process.env.ACME_TOKEN) {
-    if (acmeToken === process.env.ACME_TOKEN) {
-      acmeKey = process.env.ACME_KEY;
-    }
-  }
-
-  for (var key in process.env) {
-    if (key.startsWith('ACME_TOKEN_')) {
-      var num = key.split('ACME_TOKEN_')[1];
-      if (acmeToken === process.env['ACME_TOKEN_' + num]) {
-        acmeKey = process.env['ACME_KEY_' + num];
-      }
-    }
-  }
-
-  if (acmeKey) res.send(acmeKey);
-  else res.status(404).send();
-});
-
 app.use(express.static(root));
 app.use(fallback('index.html', { root }));
 
